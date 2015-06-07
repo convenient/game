1ff225c5006fc61537a6a792bc5d36b739df1295
convenient <lukerodgers90@gmail.com>
2015-06-07 19:48:05 +0100

    Phaser initial hacky map

--- js/game/Main.js
+++ js/game/Main.js
@@ -1,17 +1,19 @@
-var game = new Phaser.Game(800, 600, Phaser.AUTO, 'phaser-example', {
+var game = new Phaser.Game(480, 480, Phaser.AUTO, 'phaser-example', {
     preload: preload, create: create, update: update, render: render
 });
 
 function preload() {
-    game.load.image('ground_1x1', 'assets/tilemaps/tiles/ground_1x1.png');
+    game.load.image('ground_1x1', 'assets/ground_1x1.png');
+    game.load.spritesheet('player', 'assets/spaceman.png', 16, 16);
 }
 
 var map;
 var layerWalls;
+var player;
 
 function create() {
 
-    game.stage.backgroundColor = '#2d2d2d';
+    game.stage.backgroundColor = '#E2E2E2';
 
     map = game.add.tilemap();
 
@@ -29,17 +31,60 @@ function create() {
             }
         }
     }
+
+    map.setCollisionBetween(10, 10);
+
+    //  Player
+    player = game.add.sprite(48, 48, 'player', 1);
+    player.animations.add('left', [8,9], 10, true);
+    player.animations.add('right', [1,2], 10, true);
+    player.animations.add('up', [11,12,13], 10, true);
+    player.animations.add('down', [4,5,6], 10, true);
+
+    game.physics.enable(player, Phaser.Physics.ARCADE);
+
+    player.body.setSize(10, 14, 2, 1);
+
+    game.camera.follow(player);
+
+    //  Allow cursors to scroll around the map
+    cursors = game.input.keyboard.createCursorKeys();
 }
 
-function update()
-{
+function update() {
 
+    game.physics.arcade.collide(player, layerWalls);
+
+    player.body.velocity.set(0);
+
+    if (cursors.left.isDown)
+    {
+        player.body.velocity.x = -100;
+        player.play('left');
+    }
+    else if (cursors.right.isDown)
+    {
+        player.body.velocity.x = 100;
+        player.play('right');
+    }
+    else if (cursors.up.isDown)
+    {
+        player.body.velocity.y = -100;
+        player.play('up');
+    }
+    else if (cursors.down.isDown)
+    {
+        player.body.velocity.y = 100;
+        player.play('down');
+    }
+    else
+    {
+        player.animations.stop();
+    }
 }
 
 function render() {
-    game.debug.text('debug text here');
 }
-
 
 function Main()
 {
